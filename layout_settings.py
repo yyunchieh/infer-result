85b421a7dd148c00cc98e5a6fd44c2f4233061f0
Jaslene <yunchien0102@gmail.com>
2025-01-06 16:34:21 +0800

    updated

---
+++ layout_settings.py
@@ -0,0 +1,12 @@
+save_file_name = "infer_result.png"
+colors = ["#B78589", "#85B7B3"] 
+
+tick_fontsize = 12
+axis_fontsize = 18
+
+
+x_labels = ["no \n adjustment", "0.1 sec \n window size"]
+
+
+kwargs = dict()
+kwargs["tick_params"] = dict(axis="both", which="major", left=False, bottom = False)
---
+++ legend_settings.py
@@ -0,0 +1,12 @@
+legend_labels = ["p-value <= 0.05", "p-value > 0.05"]
+
+legend_loc = "center right"
+bbox_to_anchor=(1.15, 0.5)
+legend_fontsize = 14
+
+legend_kwarg = dict(labelspacing=1.0,
+                    handlelength=1.2,
+                    handleheight=1.2,
+                    handletextpad=1.5,
+                    borderpad=1.0,
+                    borderaxespad=0.0)
